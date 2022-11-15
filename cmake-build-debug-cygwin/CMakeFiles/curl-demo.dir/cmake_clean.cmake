c1ee31833bdf86f27ca0b84caf2d1bdeacaf7514
EduardoGC715 <jose.gutierrez.conejo@gmail.com>
2022-11-15 09:15:01 -0600

    file updates

---
+++ cmake-build-debug-cygwin/CMakeFiles/curl-demo.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/curl-demo.dir/contentful/contenful.cpp.o"
+  "CMakeFiles/curl-demo.dir/contentful/contenful.cpp.o.d"
+  "curl-demo.exe"
+  "curl-demo.pdb"
+  "libcurl-demo.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/curl-demo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
